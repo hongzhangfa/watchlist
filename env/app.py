03be439734e9062f93d5a53a873c2e18bc78457a
unknown <Administrator@DM-Zhangfa.cs.com.local>
2023-05-09 14:20:57 +0800

    Add minimal home page

---
+++ env/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, url_for
+
+# from flask_script import Manager
+# manager = Manager(app)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # return 'Welcome to My Watchlist!'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+from markupsafe import escape
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {name}'
+    # return f'User: {escape(name)}'
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+#自动加载代码并推送显示到浏览器
+# @manager.command
+# def dev():
+#     from livereload import Server
+#     live_server = Server(app.wsgi_app)
+#     live_server.watch("**/*.*")
+#     live_server.serve(open_url=True)
+# if __name__ == "__main__":
+#     manager.run()
